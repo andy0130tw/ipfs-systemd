0e0c28b36252f00fab3a79fc179cc8e2482cfc7d
Andy Pan <andy0130tw@yahoo.com.tw>
2020-12-04 18:19:35 +0800

    Enhance build script

--- build.sh
+++ build.sh
@@ -2,7 +2,9 @@
 
 mkdir -p dist/
 
+[ -z $(which nuitka) ] && { echo "nuitka is required but cannot be found on PATH."; exit 1; }
+
 TARGET=dist/ipfsi
-python3 /usr/bin/nuitka ./ipfsi.py -o $TARGET
+python3 $(which nuitka) ./ipfsi.py -o $TARGET
 sudo chown root:root $TARGET
 sudo chmod u+s $TARGET
